12a3d50ca20bbe421268df1337f50cc1fa3ecf87
Aitula-svg <aitu.ru00@gmail.com>
2024-08-25 20:21:28 +0300

    finished 24

--- script.js
+++ script.js
@@ -1,81 +1,166 @@
 // Задание 1
 
+const computer = [
+   { title:"Macbook Air M1",
+    price: 85000,
+   },
+   {
+    title:"Dell",
+    price:65000,
+   },
+   {
+    title:"Asus Zenbook",
+    price:45000,
+   },
+   {
+    title:"Acer Aspire",
+    price:45000,
+   },
+   {
+    title:"Macbook Pro",
+    price:100000,
+   },
+
+];
 
 
-// const student1 = {
-//   name:"Jonh",
-//   lang: "KG",
-//   age: 24
-// };
+computer.sort((prev, next) => prev.price - next.price);
+console.log(computer)
 
-// const student2 = Object.assign({},student1)
-
-// delete student2.age;
-
-// console.log(student1)
-// console.log(student2)
+computer.sort((prev, next) => next.price - prev.price);
+console.log(computer)
 
 
 // Задание 2
 
-// Global Exetusion context тузулот.
-// Жана Global Memory болот,помять ачылганда экиге болунот (steak-стек) (heap-куча).
+const grades = [4, 4, 4, 5, 3, 5, 3, 3, 2, 5, 1];
 
-// Стек (steak) - примитивный тип данныйлар жана ссылкалар сакталат.Бул жерде канча помять керек болгону алдын ала билинет жана фиксированный помять берилет.
+grades.sort((prev,next) => prev - next)
+console.log(grades)
 
+grades.sort((prev,next) => next - prev)
+console.log(grades)
 
-// Куча(heap) - не примитивный тип данныйлар объектер жана функциялар сакталат.Бул жерде канча помять керек экени белгисиз жана чексиз(без огроничений) помять болот.
 
 // Задание 3
 
+const frust = ["strawberry", "mango", "watermelon", "grapes", "lemon", "kiwi"];
 
-// const array = [
-//   1,
-//   true,
-//   3,
-//  { name: "ZH", hobbies: ["swimming","reading"] },
-//  5,
-//  "hello",
-//  "hi",
-// ];
+const frust1 = frust.sort();
+console.log(frust);
 
-// console.log(array[1]);
-// console.log(array[4]);
-// console.log(array[3].hobbies);
+const frust2 = frust.sort().reverse()
+console.log(frust)
+
+// Задание 4
+
+const numbers = [4, 123, -5, 6, 100];
+const minimum = Math.min(...numbers);
+console.log(minimum);
+
+const maximum = Math.max(...numbers);
+console.log(maximum)
 
 
-// array.push("Aiturgan")
-// array.unshift("Dansing")
-// console.log(array)
+// let minimum = numbers[0];
+// for(const  item of numbers) {
+// if(item < minimum) {
+//     minimum = item;
+// } 
+// }
 
-// array.shift()
-// console.log(array)
+// console.log(minimum)
 
-// array.indexOf("hello")
+// let maximum = numbers[0];
+// for(const item of numbers) {
+//     if(item > maximum) {
+//         maximum = item;
+//     }
+// }
 
-// const newArray = array.slice(0,3)
-
-// console.log(newArray,"newArray");
-
-// array.push("footboll");
-// console.log(array)
-
-// delete array[4,5];
-// console.log(array)
-
-// console.log(array.length);
+// console.log(maximum)
 
 
 // Задание 4
 
+// const formatedString = formatDate(new Date());...
 
-const array = [1 ,2, 3, "sdf", 4, "sdf", {}, 3, true, 2, [1, 6, 3,], 1];
 
-let  numArray = [1, 2, 3, 4, 3, 2,[1, 6, 3,], 1];
 
-const sum = numArray => numArray.reduce((res, el) => res + (Array.isArray(el) ? sum(el) : el), 0);
+// задание 5
 
-console.log(sum(numArray));
+const instagramUser = {
+    userName: "Uzumali123",
+    email:"user@gmail.com",
+    password:"123123",
+    avatarUrl:"https://www.google.com/search?q=cat",
+    followers:'1m',
+    following:512,
+    title:'Never Give up',
+};
+
+for (const key in instagramUser) {
+    console.log(key)
+}
+
+
+
+console.log(Object.values(instagramUser));
+
+// Задание 6
+
+const computers = [
+    {
+        title:"Macbool Air M1",
+        price:85000,
+    },
+    {
+        title:"Dell",
+        price:65000,
+    },
+    {
+        title:"Acer Aspire",
+        price:45000,
+    },
+    {
+        title:"Macbook Pro",
+        price:1000000
+    },
+];
+
+let sum = 0;
+for (const value of computers)
+ {
+    sum=sum+=value.price
+ }
+ console.log(sum)
+
+
+// Задание 6
+
+// const instagramUser = {
+//     userName:"Uzumaki123",
+//     email:"user@gmail.com",
+//     password:"121212",
+//     avatarUrl:
+//     "https:/www.google.com/search?q=cat",
+//     followers:'1m',
+//     following:512,
+//     title:"Never give Up",
+// };
+
+
+// for(const key in instagramUser) {
+//     console.log(key)
+// };
+
+// for(const value in instagramUser) {
+//     console.log(instagramUser[value])
+// };
+
+// for (const [key,value] of Object.entries(instagramUser)) {
+//     console.log(`${key}:${value}`)
+// }
 
 
 
